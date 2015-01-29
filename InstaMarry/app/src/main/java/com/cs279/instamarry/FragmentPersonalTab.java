2df5a8f1908ae43918c033ebc54e09719bb7357f
Justin Paul <jspaul94@gmail.com>
2015-01-28 20:13:16 -0600

    renamed fragment personal xml and making fragment tabs for fragment personal and explore different

--- InstaMarry/app/src/main/java/com/cs279/instamarry/FragmentPersonalTab.java
+++ InstaMarry/app/src/main/java/com/cs279/instamarry/FragmentPersonalTab.java
@@ -10,7 +10,7 @@ import android.widget.TextView;
 /**
  * Created by pauljs on 1/28/2015.
  */
-public class FragmentTab extends Fragment {
+public class FragmentPersonalTab extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,7 +19,7 @@ public class FragmentTab extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_layout, container, false);
+        View v = inflater.inflate(R.layout.fragment_personal_tab_layout, container, false);
         TextView tv = (TextView) v.findViewById(R.id.textView);
         tv.setText(this.getTag() + " Content");
         return v;
