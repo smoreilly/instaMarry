278970f6643d4095afdc0a19a6894c545f3b5de1
Justin Paul <jspaul94@gmail.com>
2015-01-29 10:08:29 -0600

    added tabs for explore and personal that work. Swiping is still required. Explore has a listview

--- InstaMarry/app/src/main/java/com/cs279/instamarry/FragmentExploreTab.java
+++ InstaMarry/app/src/main/java/com/cs279/instamarry/FragmentExploreTab.java
@@ -1,11 +1,19 @@
 package com.cs279.instamarry;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * Created by pauljs on 1/28/2015.
@@ -19,9 +27,68 @@ public class FragmentExploreTab extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_personal_tab_layout, container, false);
-        TextView tv = (TextView) v.findViewById(R.id.textView);
-        tv.setText(this.getTag() + " Content");
+        View v = inflater.inflate(R.layout.fragment_explore_tab_layout, container, false);
+        ListView listview = (ListView) v.findViewById(R.id.exploreListView);
+        String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
+                "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
+                "Linux", "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux",
+                "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux", "OS/2",
+                "Android", "iPhone", "WindowsMobile" };
+
+        ArrayList<String> list = new ArrayList<String>();
+        for (int i = 0; i < values.length; ++i) {
+            list.add(values[i]);
+        }
+        ArrayAdapter adapter = new StableArrayAdapter(v.getContext(),
+                android.R.layout.simple_list_item_1, list);
+        if(listview == null) {
+            System.out.println("WHY");
+        }
+        listview.setAdapter(adapter);
+
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, final View view,
+                                    int position, long id) {
+                /*final String item = (String) parent.getItemAtPosition(position);
+                view.animate().setDuration(2000).alpha(0)
+                        .withEndAction(new Runnable() {
+                            @Override
+                            public void run() {
+                                list.remove(item);
+                                adapter.notifyDataSetChanged();
+                                view.setAlpha(1);
+                            }
+                        });*/
+            }
+
+        });
         return v;
+    }
+
+    private class StableArrayAdapter extends ArrayAdapter<String> {
+
+        HashMap<String, Integer> mIdMap = new HashMap<String, Integer>();
+
+        public StableArrayAdapter(Context context, int textViewResourceId,
+                                  List<String> objects) {
+            super(context, textViewResourceId, objects);
+            for (int i = 0; i < objects.size(); ++i) {
+                mIdMap.put(objects.get(i), i);
+            }
+        }
+
+        @Override
+        public long getItemId(int position) {
+            String item = getItem(position);
+            return mIdMap.get(item);
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return true;
+        }
+
     }
 }
