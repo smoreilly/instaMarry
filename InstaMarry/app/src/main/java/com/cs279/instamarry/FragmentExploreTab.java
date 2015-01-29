4ddd47cab15d44c9c19ee78f41db29b4463b56cd
Justin Paul <jspaul94@gmail.com>
2015-01-28 20:20:39 -0600

    added fragment explore java and xml

---
+++ InstaMarry/app/src/main/java/com/cs279/instamarry/FragmentExploreTab.java
@@ -0,0 +1,27 @@
+package com.cs279.instamarry;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by pauljs on 1/28/2015.
+ */
+public class FragmentExploreTab extends Fragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_personal_tab_layout, container, false);
+        TextView tv = (TextView) v.findViewById(R.id.textView);
+        tv.setText(this.getTag() + " Content");
+        return v;
+    }
+}
