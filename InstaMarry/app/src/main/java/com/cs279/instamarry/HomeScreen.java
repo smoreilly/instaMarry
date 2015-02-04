ef733e401273b9e177d11d39d33ba7b5073eb6e1
smoreilly <smoreilly112@gmail.com>
2015-02-04 06:36:34 -0600

    Added facebook login

--- InstaMarry/app/src/main/java/com/cs279/instamarry/HomeScreen.java
+++ InstaMarry/app/src/main/java/com/cs279/instamarry/HomeScreen.java
@@ -1,12 +1,22 @@
 package com.cs279.instamarry;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Base64;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import com.facebook.*;
+import com.parse.*;
+import android.util.Log;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 
 public class HomeScreen extends ActionBarActivity {
@@ -14,47 +24,39 @@ public class HomeScreen extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Parse.initialize(this, "RqmsoL9ivWpicFS1H3ClO9VWUiPr1XmwLzJoLGRp", "qWXtduM6NlaffGawHe4CJS9aOWHtfb611KGG0oyi");
         setContentView(R.layout.activity_home_screen);
-        Button b = (Button) findViewById(R.id.button);
+        Button b = (Button) findViewById(R.id.authButton);
         b.setOnClickListener(new Button.OnClickListener() {
             public void onClick(View v)
             {
-                Intent intent = new Intent(v.getContext(), ProfileActivity.class);
-                startActivity(intent);
+                facebookLogin(v);
             }
         });
 
 
-       /* Parse.enableLocalDatastore(this);
-        Parse.initialize(this, "RqmsoL9ivWpicFS1H3ClO9VWUiPr1XmwLzJoLGRp", "qWXtduM6NlaffGawHe4CJS9aOWHtfb611KGG0oyi");
-        ParseObject gameScore = new ParseObject("GameScore");
-        gameScore.put("score", 1337);
-        gameScore.put("playerName", "Sean Plott");
-        gameScore.put("cheatMode", false);
-        gameScore.saveInBackground();
-
-        ParseQuery<ParseObject> query = ParseQuery.getQuery("GameScore");
-        query.getInBackground("xWMyZ4YEGZ", new GetCallback<ParseObject>() {
-            public void done(ParseObject object, ParseException e) {
-                if (e == null) {
-                    // object will be your game score
-                } else {
-                    // something went wrong
-                }
-            }
-        });
-
-        int score = gameScore.getInt("score");
-        String playerName = gameScore.getString("playerName");
-        boolean cheatMode = gameScore.getBoolean("cheatMode");
-
-        Log.i("First", Integer.toString(score));
-        Log.i("Second", playerName);
-        Log.i("Third",Boolean.toString(cheatMode));*/
 
     }
 
 
+    public void facebookLogin(final View v){
+        ParseFacebookUtils.logIn(this, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException err) {
+                if (user == null) {
+                    Log.d("MyApp", "Uh oh. The user cancelled the Facebook login.");
+                } else if (user.isNew()) {
+                    Log.d("MyApp", "User signed up and logged in through Facebook!");
+                    Intent intent = new Intent(v.getContext(), ProfileActivity.class);
+                    startActivity(intent);
+                } else {
+                    Log.d("MyApp", "User logged in through Facebook!");
+                    Intent intent = new Intent(v.getContext(), ProfileActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -79,4 +81,12 @@ public class HomeScreen extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        ParseFacebookUtils.finishAuthentication(requestCode, resultCode, data);
+    }
+
+
 }
