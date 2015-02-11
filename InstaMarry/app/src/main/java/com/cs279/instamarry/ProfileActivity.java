90609d6e348eb3c5698619a3cbfac881a2731cc7
smoreilly <smoreilly112@gmail.com>
2015-02-10 19:27:58 -0600

    Add button launches to camera roll

--- InstaMarry/app/src/main/java/com/cs279/instamarry/ProfileActivity.java
+++ InstaMarry/app/src/main/java/com/cs279/instamarry/ProfileActivity.java
@@ -1,12 +1,16 @@
 package com.cs279.instamarry;
 
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.content.Intent;
 
 //http://developer.android.com/training/implementing-navigation/lateral.html#horizontal-paging
 
@@ -75,4 +79,36 @@ public class ProfileActivity extends ActionBarActivity implements ActionBar.TabL
     public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
     }
 
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_profile, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.add){
+            Log.d("Profile Activity", "Add button workoing");
+            int RESULT_GALLERY = 0;
+
+            Intent galleryIntent = new Intent(
+                    Intent.ACTION_PICK,
+                    android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(galleryIntent , RESULT_GALLERY );
+            return true;
+        }
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
