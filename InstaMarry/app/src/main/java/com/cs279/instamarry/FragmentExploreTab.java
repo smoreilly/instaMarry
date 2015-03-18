bb954625acbe3a46dd9459b76f42b9cd3698f978
smoreilly <smoreilly112@gmail.com>
2015-03-18 14:27:46 -0500

    Added Explore Tab

--- InstaMarry/app/src/main/java/com/cs279/instamarry/FragmentExploreTab.java
+++ InstaMarry/app/src/main/java/com/cs279/instamarry/FragmentExploreTab.java
@@ -1,35 +1,41 @@
 package com.cs279.instamarry;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.pm.ResolveInfo;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.view.ActionMode;
+import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.Checkable;
-import android.widget.FrameLayout;
-import android.widget.GridView;
-import android.widget.ImageView;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
+import com.activeandroid.query.Select;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+import com.parse.ParseFile;
+
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import butterknife.ButterKnife;
+import butterknife.InjectView;
+import rx.Observable;
+import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 /**
  * Created by pauljs on 1/28/2015.
- * Aid from: http://www.java2s.com/Code/Android/UI/ThisdemoillustratestheuseofCHOICEMODEMULTIPLEMODALakaselectionmodeonGridView.htm
  */
 public class FragmentExploreTab extends Fragment {
-    private List<ResolveInfo> mApps;
-    private View v;
-    private GridView gridview;
-    //TODO
+    @InjectView(R.id.exploreListView1) ListView list;
+    LazyAdapter adapter;
+    private List<Post> songsList;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -39,114 +45,91 @@ public class FragmentExploreTab extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        v = inflater.inflate(R.layout.fragment_explore_tab_layout, container, false);
+
+        View v = inflater.inflate(R.layout.fragment_explore_tab_layout, container, false);
         ButterKnife.inject(this, v);
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                Intent intent = new Intent(getActivity(), DetailedItem.class);
+                intent.putExtra("id", songsList.get(position).getMy_post_id());
+                //start detaileditem without delete or edit options!!!
+            }
+        });
+        songsList = new ArrayList<Post>();
+
+        //TODO fix this so is only updates on pull to refresh and when first created. Not on every screen change
+
+        List<Post> pList = new Select().
+                from(Post.class).
+                where("UserId = ?", ParseUser.getCurrentUser().
+                        getObjectId())
+                .execute();
+        for(Post p: pList) p.delete();
+        Log.i("Pulling From Parse", "Pulling");
+        pullFromParseWithRXJava();
         return v;
     }
 
-    private void loadApps() {
-        Intent mainIntent = new Intent(Intent.ACTION_MAIN, null);
-        mainIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+    private void pullFromParseWithRXJava(){
 
-        mApps = v.getContext().getPackageManager().queryIntentActivities(mainIntent, 0);
-    }
+        Observable.from(ParseUser.getCurrentUser().getList("following"))
+                .flatMap(userId ->
+                    Observable.from(getUserPosts(userId.toString())).subscribeOn(Schedulers.io()))
+                .toSortedList((p1,p2) -> p2.getUpdatedAt().compareTo(p1.getUpdatedAt()))
+                .flatMap(Observable::from)
+                .take(10)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Subscriber<ParseObject>() {
+                    @Override
+                    public void onCompleted() {
+                        adapter = new LazyAdapter(getActivity(), songsList);
+                        list.setAdapter(adapter);
+                    }
 
-    public class AppsAdapter extends BaseAdapter {
-        public AppsAdapter() {
-        }
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.e("SO", "Error in explore tab");
+                    }
 
-        public View getView(int position, View convertView, ViewGroup parent) {
-            CheckableLayout l;
-            ImageView i;
+                    @Override
+                    public void onNext(ParseObject p) {
+                        Post post = new Post(p.getObjectId(),
+                                p.getString("title"),
+                                p.getString("description"),
+                                p.getString("time"),
+                                p.getString("userId"),
+                                ((ParseFile) p.get("postImage")).getUrl());
+                        post.save();
+                        songsList.add(post);
+                    }
+                });
 
-            if (convertView == null) {
-                i = new ImageView(v.getContext());
-                i.setScaleType(ImageView.ScaleType.FIT_CENTER);
-                i.setLayoutParams(new ViewGroup.LayoutParams(50, 50));
-                l = new CheckableLayout(v.getContext());
-                l.setLayoutParams(new GridView.LayoutParams(GridView.LayoutParams.WRAP_CONTENT,
-                        GridView.LayoutParams.WRAP_CONTENT));
-                l.addView(i);
-            } else {
-                l = (CheckableLayout) convertView;
-                i = (ImageView) l.getChildAt(0);
-            }
-
-            ResolveInfo info = mApps.get(position);
-            i.setImageDrawable(info.activityInfo.loadIcon(v.getContext().getPackageManager()));
-
-            return l;
-        }
-
-
-        public final int getCount() {
-            return mApps.size();
-        }
-
-        public final Object getItem(int position) {
-            return mApps.get(position);
-        }
-
-        public final long getItemId(int position) {
-            return position;
-        }
-    }
-
-    public class CheckableLayout extends FrameLayout implements Checkable {
-        private boolean mChecked;
-
-        public CheckableLayout(Context context) {
-            super(context);
-        }
-
-        public void setChecked(boolean checked) {
-            mChecked = checked;
-            setBackgroundDrawable(checked ?
-                    getResources().getDrawable(R.drawable.profile_pic_placeholder)
-                    : null);
-        }
-
-        public boolean isChecked() {
-            return mChecked;
-        }
-
-        public void toggle() {
-            setChecked(!mChecked);
-        }
 
     }
 
-    public class MultiChoiceModeListener implements GridView.MultiChoiceModeListener {
-        public boolean onCreateActionMode(ActionMode mode, Menu menu) {
-            mode.setTitle("Select Items");
-            mode.setSubtitle("One item selected");
-            return true;
-        }
 
-        public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
-            return true;
+    private List<ParseObject> getUserPosts(String id){
+        try {
+            Log.i("SO: id of following", id);
+            ParseQuery<ParseObject> query = ParseQuery.getQuery("Post");
+            query.whereEqualTo("userId", id);
+            return query.find();
+        }catch (ParseException err){
+            throw new RuntimeException();
         }
-
-        public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
-            return true;
-        }
-
-        public void onDestroyActionMode(ActionMode mode) {
-        }
-
-        public void onItemCheckedStateChanged(ActionMode mode, int position, long id,
-                                              boolean checked) {
-            int selectCount = gridview.getCheckedItemCount();
-            switch (selectCount) {
-                case 1:
-                    mode.setSubtitle("One item selected");
-                    break;
-                default:
-                    mode.setSubtitle("" + selectCount + " items selected");
-                    break;
-            }
-        }
-
     }
+
+    public void addPost() {
+        Log.i("TEST FOR CURSOR WINDOW", "BLAH");
+        songsList = new Select().from(Post.class).execute();
+        Log.i("TEST FOR CURSOR WINDOW", "BLAH2");
+        adapter = new LazyAdapter(getActivity(), songsList);
+        list.setAdapter(adapter);
+    }
+
 }
-
